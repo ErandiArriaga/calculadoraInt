5140c6e031a40b00ed42c708c770a3f16e4c0f7d
ensamblaTec <net.git.ketza@gmail.com>
2023-09-13 18:57:46 -0600

    Ultimos cambios, ya jala xd

--- src/main/java/com/example/calculadora/HelloApplication.java
+++ src/main/java/com/example/calculadora/HelloApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
@@ -13,7 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 950, 600);
         scene.getStylesheets().add(
-                getClass().getResource("css/style.css").toString()
+                Objects.requireNonNull(getClass().getResource("css/style.css")).toString()
         );
         stage.setTitle("Calculo de Interés");
         stage.setScene(scene);
--- src/main/java/com/example/calculadora/HelloController.java
+++ src/main/java/com/example/calculadora/HelloController.java
@@ -4,27 +4,64 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 
 public class HelloController {
+
+
     @FXML
     private Button btnSimple;
 
+    // Primeros botones
+    @FXML
+    TextField txtSumaPrincipal, txtTasaInteres, txtPeriodoTiempo;
+
+    @FXML
+    TextField txtCantidadPrincipal,txtTasaInteres2,txtPeriodoTiempo2;
+
+    @FXML
+    TextField txtCantidadPrincipal2, txtCantidadCompuesta;
+
     @FXML
     protected void onSimpleButtonClick() {
-        showMessage("El interés simple es de:");
+        try{
+            float suma_principal = Float.parseFloat(txtSumaPrincipal.getText());
+            float tasa_de_interes = Float.parseFloat(txtTasaInteres.getText());
+            float periodo_tiempo = Float.parseFloat(txtPeriodoTiempo.getText());
+            float res = (suma_principal * tasa_de_interes * periodo_tiempo) / 100;
+            showMessage("El interés simple es de :"+res);
+        }catch (Exception err){
+            showMessage("Verifica los datos");
+        }
+
     }
 
     @FXML
     protected void onCompuestoButtonClick() {
-
-        showMessage("El interés compuesto es de:");
+        try{
+            float cantidad_principal = Float.parseFloat(txtCantidadPrincipal.getText());
+            float tasa_de_interes = Float.parseFloat(txtTasaInteres2.getText());
+            float periodo_tiempo = Float.parseFloat(txtPeriodoTiempo2.getText());
+            float res = (float) (cantidad_principal * (Math.pow(( 1 + tasa_de_interes / 100), periodo_tiempo)));
+            showMessage("El interés compuesto es de:: "+res);
+        }catch (Exception err){
+            showMessage("Verifica los datos");
+        }
     }
 
     @FXML
     protected void onINTCButtonClick() {
+        try{
+            float cantidad_compuesta = Float.parseFloat(txtCantidadCompuesta.getText());
+            float cantidad_principal = Float.parseFloat(txtCantidadPrincipal2.getText());
+            float res = cantidad_compuesta - cantidad_principal;
+            showMessage("El interés compuesto al final del año(s) es de: "+res);
+        }catch (Exception err){
+            showMessage("Verifica los datos");
+        }
 
-        showMessage("El interés compuesto al final del año(s) es de:");
     }
 
     private void showMessage (String message){
